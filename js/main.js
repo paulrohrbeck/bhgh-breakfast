6e6cfde509ceab2826ca411bbe01b47b4fba720c
Paul Rohrbeck <paul@infielddesign.com>
2014-02-25 17:01:24 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('.navbar a, .register-button').click(function() {
+        var elementClicked = $(this).attr("href");
+        var destination = $(elementClicked).offset().top;
+        $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-15}, 500 );
+        return false;
+    });
+
+});
