7051e69ae1ad6ec7205b1a66133692dfd8b34961
Paul Rohrbeck <paul@infielddesign.com>
2014-02-26 16:10:50 -0800

    register form updated

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 
+    // scroll to:
     $('.navbar a, .register-button').click(function() {
         var elementClicked = $(this).attr("href");
         var destination = $(elementClicked).offset().top;
         $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-15}, 500 );
         return false;
+    });
+
+    // register button:
+    $('#pay-now').click(function(){
+        $('#register-form').toggleClass('hidden');
     });
 
 });
